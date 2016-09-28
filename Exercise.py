bc62d7ea95b33ab2db5dadf592f4bced98b3c296
16ndellorco <16ndellorco@gmail.com>
2016-09-28 16:29:03 -0400

    assignment 1

--- Exercise.py
+++ Exercise.py
@@ -1,7 +1,44 @@
-# example 3
-
-password = input('Enter the password:')
-if password == 'prospero':
-    print ('password accepted.')
+unit = (input("Are you using USC (US Customary Units)? "))
+if unit in ['yes', 'Yes']:
+    distance = int(input("How many miles did you drive? "))
+    gas = int(input("How many gallons of gas did you use? "))
+    kilometers = distance * 1.60934
+    liters = gas*3.78541
+    cm = liters/(kilometers*100)
+    if cm > 20:
+        GCR = "extremely poor"
+    elif cm > 15:
+        GCR = "poor"
+    elif cm > 10:
+        GCR = "average"
+    elif cm > 8:
+        GCR = "good"
+    else:
+        GCR = "excellent"
+    print("Distance:____: ", format(distance, '.3f'), "Miles", format(kilometers, '.3f'), "Km", sep=' ')
+    print("Gas:_________: ", format(gas, '.3f'), "Gallons", format(liters, '.3f'), "Liters", sep=' ')
+    print("Consumption__: ", format(distance/gas, '.3f'), "mpg", format(kilometers/liters, '.3f'), "L/100km", sep='')
+    print("Your Gas Consumption Rating is", GCR)
+elif unit in ['no', 'No']:
+    distance = int(input("How many kilometers did you drive? "))
+    gas = int(input("How many Liters of gas did you use? "))
+    miles = distance * .621371
+    gallons = gas * .264172
+    cm = gas/(distance*100)
+    if cm > 20:
+        GCR = "extremely poor"
+    elif cm > 15:
+        GCR = "poor"
+    elif cm > 10:
+        GCR = "average"
+    elif cm > 8:
+        GCR = "good"
+    else:
+        GCR = "excellent"
+    print("Distance _____:", format(miles, '.3f'), "Miles", format(distance, '.3f'), "Km", sep=' ')
+    print("Gas __________:", format(gallons, '.3f'), "Gallons", format(gas, '.3f'), "Liters", sep=' ')
+    print("Consumption __:", format(miles / gallons, '.3f'), "mpg", format(100*(gas/distance), '.3f'),\
+          "L/100km", sep=' ')
+    print("Your Gas Consumption Rating is", GCR)
 else:
-    print('Sorry, that is the wrong password')
+    print("""Please enter either "Yes" or "No""")
--- nid45/assignment 1.py
+++ nid45/assignment 1.py
@@ -1,10 +1,10 @@
-unit = (input("Are you using USC? "))
+unit = (input("Are you using USC (US Customary Units)? "))
 if unit in ['yes', 'Yes']:
-    distance = int(input("How many miles did you drive? "))
-    gas = int(input("How many gallons of gas did you use? "))
+    distance = float(input("How many miles did you drive? "))
+    gas = float(input("How many gallons of gas did you use? "))
     kilometers = distance * 1.60934
     liters = gas*3.78541
-    cm = (liters/100)*kilometers
+    cm = (liters*100)/kilometers
     if cm > 20:
         GCR = "extremely poor"
     elif cm > 15:
@@ -15,16 +15,16 @@ if unit in ['yes', 'Yes']:
         GCR = "good"
     else:
         GCR = "excellent"
-    print("Distance: ", format(distance, '.3f'), "Miles", format(kilometers, '.3f'), "Km", sep=' ')
-    print("Gas: ", format(gas, '.3f'), "Gallons", format(liters, '.3f'), "Liters", sep=' ')
-    print("Consumption: ", format(distance/gas, '.3f'), "mpg", format(kilometers/liters, '.3f'), "L/100km", sep='')
+    print("Distance:____: ", format(distance, '.3f'), "Miles", format(kilometers, '.3f'), "Km", sep=' ')
+    print("Gas:_________: ", format(gas, '.3f'), "Gallons", format(liters, '.3f'), "Liters", sep=' ')
+    print("Consumption__: ", format(distance/gas, '.3f'), "mpg", format(100*liters/kilometers, '.3f'), "L/100km", sep='')
     print("Your Gas Consumption Rating is", GCR)
 elif unit in ['no', 'No']:
-    distance = int(input("How many kilometers did you drive? "))
-    gas = int(input("How many Liters of gas did you use? "))
+    distance = float(input("How many kilometers did you drive? "))
+    gas = float(input("How many Liters of gas did you use? "))
     miles = distance * .621371
     gallons = gas * .264172
-    cm = (gas / 100) * distance
+    cm = gas * (distance*100)
     if cm > 20:
         GCR = "extremely poor"
     elif cm > 15:
@@ -35,9 +35,10 @@ elif unit in ['no', 'No']:
         GCR = "good"
     else:
         GCR = "excellent"
-    print("Distance: ", format(miles, '.3f'), "Miles", format(distance, '.3f'), "Km", sep=' ')
-    print("Gas: ", format(gallons, '.3f'), "Gallons", format(gas, '.3f'), "Liters", sep=' ')
-    print("Consumption: ", format(miles / gallons, '.3f'), "mpg", format(distance / gas, '.3f'), "L/100km", sep='')
+    print("Distance _____:", format(miles, '.3f'), "Miles", format(distance, '.3f'), "Km", sep=' ')
+    print("Gas __________:", format(gallons, '.3f'), "Gallons", format(gas, '.3f'), "Liters", sep=' ')
+    print("Consumption __:", format(miles / gallons, '.3f'), "mpg", format(100*(gas/distance), '.3f'),\
+          "L/100km", sep=' ')
     print("Your Gas Consumption Rating is", GCR)
 else:
     print("""Please enter either "Yes" or "No""")
