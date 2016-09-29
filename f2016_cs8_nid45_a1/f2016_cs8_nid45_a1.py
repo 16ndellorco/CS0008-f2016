10801a98844d09506a9acc21da3ce59b61a23f73
16ndellorco <16ndellorco@gmail.com>
2016-09-29 14:50:08 -0400

    assignment 1

--- f2016_cs8_nid45_a1/f2016_cs8_nid45_a1.py
+++ f2016_cs8_nid45_a1/f2016_cs8_nid45_a1.py
@@ -15,9 +15,9 @@ if unit in ['yes', 'Yes']:
         GCR = "good"
     else:
         GCR = "excellent"
-    print("Distance:____: ", format(distance, '.3f'), "Miles", format(kilometers, '.3f'), "Km", sep=' ')  #print in the needed format with 3 columns, (ask how to do columns correctly)
-    print("Gas:_________: ", format(gas, '.3f'), "Gallons", format(liters, '.3f'), "Liters", sep=' ')
-    print("Consumption__:", format(distance/gas, '.3f'), " mpg", format(100*liters/kilometers, '.3f'), "L/100km", sep=' ')
+    print("Distance:____: ", format(distance, '12.3f'), "Miles", format(kilometers, '22.3f'), "Km", sep=' ')  #print in the needed format with 3 columns, (ask how to do columns correctly)
+    print("Gas:_________: ", format(gas, '12.3f'), "Gallons", format(liters, '20.3f'), "Liters", sep=' ')
+    print("Consumption__:", format(distance/gas, '13.3f'), "mpg", format(100*liters/kilometers, '23.3f'), "L/100km", sep=' ')
     print("Your Gas Consumption Rating is", GCR)
 elif unit in ['no', 'No']:  #if user says no they want to input information in metric system
     distance = float(input("How many kilometers did you drive? "))  #ask in terms of kilometers/liters
@@ -35,10 +35,10 @@ elif unit in ['no', 'No']:  #if user says no they want to input information in m
         GCR = "good"
     else:
         GCR = "excellent"
-    print("Distance _____:", format(miles, '.3f'), "Miles", format(distance, '.3f'), "Km", sep=' ')
-    print("Gas __________:", format(gallons, '.3f'), "Gallons", format(gas, '.3f'), "Liters", sep=' ')
-    print("Consumption __:", format(miles / gallons, '.3f'), "mpg",\
-          format(100*(gas/distance), '.3f'), "L/100km", sep=' ')
+    print("Distance _____:", format(miles, '12.3f'), "Miles", format(distance, '24.3f'), "Km", sep=' ')
+    print("Gas __________:", format(gallons, '12.3f'), "Gallons", format(gas, '22.3f'), "Liters", sep=' ')
+    print("Consumption __:", format(miles / gallons, '12.3f'), "mpg",\
+          format(100*(gas/distance), '26.3f'), "L/100km", sep=' ')
     print("Your Gas Consumption Rating is", GCR) #give rating based on given data
 else:
     print("""Please enter either "Yes" or "No""")  #user must say yes or no to initial question
--- f2016_cs8_nid45_a1/no comments.py
+++ f2016_cs8_nid45_a1/no comments.py
@@ -15,9 +15,9 @@ if unit in ['yes', 'Yes']:
         GCR = "good"
     else:
         GCR = "excellent"
-    print("Distance:____: ", format(distance, '.3f'), "Miles", format(kilometers, '.3f'), "Km", sep=' ')
-    print("Gas:_________: ", format(gas, '.3f'), "Gallons", format(liters, '.3f'), "Liters", sep=' ')
-    print("Consumption__:", format(distance/gas, '.3f'), " mpg", format(100*liters/kilometers, '.3f'), "L/100km", sep=' ')
+    print("Distance:____: ", format(distance, '12.3f'), "Miles", format(kilometers, '24.3f'), "Km")
+    print("Gas:_________: ", format(gas, '12.3f'), "Gallons", format(liters, '22.3f'), "Liters")
+    print("Consumption__: ", format(distance/gas, '12.3f'), "mpg", format(100*liters/kilometers, '26.3f'), "L/100km")
     print("Your Gas Consumption Rating is", GCR)
 elif unit in ['no', 'No']:
     distance = float(input("How many kilometers did you drive? "))
@@ -35,10 +35,10 @@ elif unit in ['no', 'No']:
         GCR = "good"
     else:
         GCR = "excellent"
-    print("Distance _____:", format(miles, '.3f'), "Miles", format(distance, '.3f'), "Km", sep=' ')
-    print("Gas __________:", format(gallons, '.3f'), "Gallons", format(gas, '.3f'), "Liters", sep=' ')
-    print("Consumption __:", format(miles / gallons, '.3f'), "mpg",
-          format(100*(gas/distance), '.3f'), "L/100km", sep=' ')
+    print("Distance _____:", format(miles, '12.3f'), "Miles", format(distance, '24.3f'), "Km")
+    print("Gas __________:", format(gallons, '12.3f'), "Gallons", format(gas, '22.3f'), "Liters")
+    print("Consumption __:", format(miles / gallons, '12.3f'), "mpg",
+          format(100*(gas/distance), '26.3f'), "L/100km")
     print("Your Gas Consumption Rating is", GCR)
 else:
     print("""Please enter either "Yes" or "No""")
