6b09f9128fd0bbd1cbf4b70e89e9131a46aa3d5e
16ndellorco <16ndellorco@gmail.com>
2016-10-29 16:47:51 -0400

    Add files via upload

--- Assignment 2.py
+++ Assignment 2.py
@@ -1,10 +1,8 @@
-
 # printKV: prints and formats key and values based on data type
 def printKV(key, value, klen = 0):
-    # print simple strings without value
+    # prints simple strings without value
     if value == None:
         print(key)
-
     # check for different data types
     if isinstance(value, str):
         print('{:<{}}: {:>20}'.format(key, len(key) + klen, value))
@@ -12,67 +10,66 @@ def printKV(key, value, klen = 0):
         print('{:<{}}: {:>10.3f}'.format(key, len(key) + klen, value))
     elif isinstance(value, int):
         print('{:<{}}: {:>10}'.format(key, len(key) + klen, value))
-
 # processFile: process a file and reads and loops through all lines of file handled
 def processFile(fh):
     # read all lines and store them in a variable
     lines = fh.readlines()
     # track how many lines there're in the file
-    lineCount = 0    # track total distance and line count
-    totalDistance = 0
-
-    for l in lines: # loop through each line in the file
+    linecount = 0
+    # track total distance
+    totaldistance = 0
+    # loop through each line in the file
+    for l in lines:
         # remove new line character
         l = l.rstrip('\n')
         # split name and distance
         info = l.split(',')
-        # only process lines where a name and distance are found
+        # only process lines where a name and
+        # distance are found
         if len(info) == 2:
             # set distance variable from value in file
             distance = float(info[1])
-            if isinstance(distance, float):  # make sure distance is a float
-                totalDistance += distance
-            lineCount += 1  # increment line count, increases by 1 at at time
-    return (lineCount, totalDistance)  # return two values, line count and total distance
-
-def summary(totalLineCount, totalDistance):
+            # make sure distance is a float
+            if isinstance(distance, float):
+                totaldistance += distance
+            # increment line count, increases by 1 at a time
+            linecount += 1
+    # returns two values, line count and total distance
+    return (linecount, totaldistance)
+def summary(totallinecount, totaldistance):
     printKV('Totals', None)
-    printKV('Total # of lines', totalLineCount,  8)
-    printKV('Total distance run', totalDistance, 6)
+    printKV('Total # of lines', totallinecount,  8)
+    printKV('Total distance run', totaldistance, 6)
 # main function this is the first entry into the program
 def main():
-
-    FinalLineCount = 0
-    FinalDistance  = 0
-
-    while True:     # loop to keep asking for files to read
+    finallinecount = 0
+    finaldistance  = 0
+    # loop to keep asking for files to read
+    while True:
         # gets the file name to be read
         fileName = input('File to be read: ')
-
-        if fileName.lower() == 'quit':   # if user enters quit, display totals
+        # if user enters quit, display totals
+        if fileName.lower() == 'quit':
             # display of totals
-            summary(FinalLineCount, FinalDistance)
-            break  #terminates current function
-
-        # validate input by making sure length is > 0 and it is a string variable otherwise display invalid input
+            summary(finallinecount, finaldistance)
+            break
+# validate input by making sure length is > 0 and it is a string variable otherwise display invalid input
         if not len(fileName) == 0 and not isinstance(fileName, str):
             print('Invalid input.')
             continue
         else:
-            # open file by using open function
+            # open file by using the open function
             with open(fileName, 'r') as fileHandle:
-                # get line count and total distance
+                # find the total line count and total distance
                 values = processFile(fileHandle)
-
-                # print line count and distance
+                # prints the line count and distance
                 printKV('Partial Total # of lines',values[0])
                 printKV('Partial distance run',values[1], 4)
-
                 # add totals to grand total
-                FinalLineCount += values[0]
-                FinalDistance  += values[1]
-                print(end='\n') # add a new line at the end
+                finallinecount += values[0]
+                finaldistance  += values[1]
             # file is automatically closed when using the keyword
-
+            print(end='\n')
 if __name__ == "__main__":
-    main()    # call the main function
+    # call the main function
+    main()
